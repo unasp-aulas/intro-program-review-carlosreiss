6b9e5e79059faf16f96b1f0223b1691de4ed9506
carlosreiss <167372962+carlosreiss@users.noreply.github.com>
2024-06-12 19:57:35 -0300

    questao 2

---
+++ problem_2.py
@@ -0,0 +1,10 @@
+def soma_sequencia_ate_n(ultimo):
+    return list(range(1, ultimo + 1))
+
+def soma(main):
+    return sum(main)
+
+ultimo = 99
+sequencia = soma_sequencia_ate_n(ultimo)
+
+#print(sequencia)
