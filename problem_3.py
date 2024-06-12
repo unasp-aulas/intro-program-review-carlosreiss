1b2a298935601cf2eef9313da8ee49d597a123cb
carlosreiss <167372962+carlosreiss@users.noreply.github.com>
2024-06-12 20:02:42 -0300

    questa3.2

--- problem_3.py
+++ problem_3.py
@@ -1,7 +1,7 @@
 soma = 0
 
 while True:
-    numero = float(input('Escreva um nuemro (0 para sair): '))
+    numero = float(input('Escreva um numero (0 para sair): '))
 
     if numero == 0:
         break
