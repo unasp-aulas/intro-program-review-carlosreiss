7cb63d74444f94851f05028e33ef45f4b081a0ab
carlosreiss <167372962+carlosreiss@users.noreply.github.com>
2024-06-12 19:50:21 -0300

    questao 1.2

--- problem_1.py
+++ problem_1.py
@@ -3,9 +3,9 @@ def main(cargo, salario):
     if cargo == 'junior':
         aumento = salario * 0.15
     elif cargo == 'senior':
-        aumento = salario * 0.26
-    elif cargo == 'pleno':
         aumento = salario * 0.34
+    elif cargo == 'pleno':
+        aumento = salario * 0.26
     else:
         return -1
 
