0efb876142796821d279750343e8c9f91118b962
carlosreiss <167372962+carlosreiss@users.noreply.github.com>
2024-06-12 19:59:18 -0300

    questao 4

--- problem_4.py
+++ problem_4.py
@@ -1,3 +1,14 @@
 valor_casa = float(input("Qual o valor da casa? "))  # Não alterar
 salario = float(input("Qual é o salário? "))  # Não alterar
 anos_pagar = int(input("Pagar em quantos anos? "))  # Não alterar
+
+valor_casa = float(input("Qual o valor da casa? "))
+salario = float(input("Qual é o salário? "))
+anos_pagar = int(input("Pagar em quantos anos? "))
+
+prestação = valor_casa // anos_pagar
+
+if prestação > 0.30 * salario:
+    print("Desaprovado")
+elif prestação < 0.30 * salario:
+    print('Aprovado')
